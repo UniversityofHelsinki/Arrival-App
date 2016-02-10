fee54972f2b0f35a9f389ab1410d57aaa3f7945f
Teemu Pärssinen <teemu.parssinen@wunderkraut.com>
2016-02-10 10:36:18 +0200

    Fix guide hiding based on selections

--- arrival/src/app/components/answers/answers.service.js
+++ arrival/src/app/components/answers/answers.service.js
@@ -7,7 +7,9 @@
 
   /** @ngInject */
   function answersService($log, $http) {
-    var apiHost = '/api/answer';
+    var apiHost = '/api/';
+    var listHidden = [];
+    var listGuides = [];
 
     var service = {
       apiHost: apiHost,
@@ -17,16 +19,61 @@
     return service;
 
     function getAnswer(selection) {
-      return $http.get(apiHost + '/' + selection)
+      getHidden(selection);
+      return $http.get(apiHost + 'answer/' + selection)
         .then(getAnswerComplete)
         .catch(getAnswerFailed);
 
+      function getHidden(selection) {
+        return $http.get(apiHost + 'hide/' + selection)
+          .success(function(data) {
+            listHidden = data;
+            $log.debug('1:');
+            $log.debug(listHidden);
+          });
+      }
+
       function getAnswerComplete(response) {
-        return response.data;
+        var i = 0;
+        $log.debug('2:');
+        $log.debug(listHidden);
+        if(listHidden.length > 0) {
+          angular.forEach(response.data, function() {
+            if (!isHidden(response.data[i].nid)) {
+              $log.debug('response.data: ');
+              $log.debug(response.data);
+              $log.debug('response.data[i]: ');
+              $log.debug(response.data[i]);
+              listGuides.push(response.data[i]);
+            }
+            i++;
+          });
+        } else {
+          listGuides = response.data;
+        }
+
+        function isHidden(aid) {
+          var i = 0;
+          var hidden = false;
+          $log.debug('3:');
+          $log.debug(listHidden);
+          angular.forEach(listHidden, function() {
+            $log.debug('check!!')
+            if (aid == listHidden[i].nid) {
+              $log.debug('hide!!')
+              hidden = true;
+            }
+            i++;
+          });
+          return hidden;
+        }
+
+        $log.debug('listGuides: ' + listGuides);
+        return listGuides;
       }
 
       function getAnswerFailed(error) {
-        $log.error('XHR Failed for getanswers.\n' + angular.toJson(error.data, true));
+        $log.error('XHR Failed for getAnswers.\n' + angular.toJson(error.data, true));
       }
     }
   }
