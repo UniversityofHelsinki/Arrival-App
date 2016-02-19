fd4a2afc01b785c8d7ad35ee8bf55aafe64da098
Teemu Merikoski <teemu.merikoski@wunderkraut.com>
2016-02-19 13:42:19 +0200

    Production build functionality with settings

---
+++ conf/prod.settings.php
@@ -0,0 +1,17 @@
+<?php
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'dfhgjw78yrg078',
+  'host' => 'localhost',
+  'collation' => 'utf8mb4_swedish_ci',
+);
+
+$conf['drupal_http_request_fails'] = FALSE;
+
+$settings['install_profile'] = 'config_installer';
+$config_directories['sync'] = DRUPAL_ROOT . '/../config/sync';
+$settings['hash_salt'] = '_Yfo_5ZbqO2oOOrypjh7QlMMmnpkeA2_Egvdn09ZTAa3ajv19494SKvJ06NZcsdb_2piGYqqGA';
+
