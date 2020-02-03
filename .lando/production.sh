d082c8999a8ed28266b882a909e20dc0efcc3cad
Tormi Tabor <tormi.tabor@gmail.com>
2020-02-03 23:27:31 +0200

    Reflect file structure changes (drupal > web) in production script.

--- .lando/production.sh
+++ .lando/production.sh
@@ -10,13 +10,13 @@ mkdir -p /app/production/drupal/app
 mkdir -p /app/production/drupal/sites/default
 
 cp -a /app/composer.json /app/production/composer.json
-cp -a /app/drupal/modules/custom/ /app/production/drupal/modules/custom/
-cp -a /app/drupal/themes/custom/ /app/production/drupal/themes/custom/
+cp -a /app/web/modules/custom/ /app/production/drupal/modules/custom/
+cp -a /app/web/themes/custom/ /app/production/drupal/themes/custom/
 cp -a /app/config/sync/ /app/production/config/
 cp -a /app/arrival/dist/ /app/production/drupal/app/
-cp -a /app/drupal/.htaccess /app/production/drupal/.htaccess
-cp -a /app/drupal/index.php /app/production/drupal/index.php
-cp -a /app/drupal/autoload.php /app/production/drupal/autoload.php
+cp -a /app/web/.htaccess /app/production/drupal/.htaccess
+cp -a /app/web/index.php /app/production/drupal/index.php
+cp -a /app/web/autoload.php /app/production/drupal/autoload.php
 cp -a /app/conf/prod.settings.php /app/production/drupal/sites/default/settings.php
 cd /app/production/drupal/sites/default
 ln -sf ../../../files files
