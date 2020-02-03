426937d6cddf149e178384f1fa3e961ab8316db0
Tormi Tabor <tormi.tabor@gmail.com>
2020-02-03 22:36:40 +0200

    Update config_sync_directory & install_profile.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,6 +1,16 @@
 <?php
 
 /**
+ * Config directory path.
+ */
+$settings['config_sync_directory'] = '../config/sync';
+
+/**
+ * Install profile.
+ */
+$settings['install_profile'] = 'config_installer';
+
+/**
  * Environment specific override configuration, if available.
  */
 if (file_exists(__DIR__ . '/settings.local.php')) {
