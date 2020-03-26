1f0ba07aa4a203f35490f5cbdda328ec6b45a6b4
Tormi Tabor <tormi.tabor@gmail.com>
2020-03-26 12:29:02 +0200

    HA-15: Drupal 8 no longer uses the $settings['install_profile'] value in settings.php.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -6,11 +6,6 @@
 $settings['config_sync_directory'] = '../config/sync';
 
 /**
- * Install profile.
- */
-$settings['install_profile'] = 'config_installer';
-
-/**
  * Environment specific override configuration, if available.
  */
 if (file_exists(__DIR__ . '/settings.local.php')) {
