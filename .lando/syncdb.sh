db86c20bb233a14d2e95c8293de346a496d4988e
Tormi Tabor <tormi.tabor@gmail.com>
2021-06-01 15:12:45 +0300

    HYAA-22: Simplify Drush aliases setup.

--- .lando/syncdb.sh
+++ .lando/syncdb.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 set -exu
 
-# Synchronise local database with selected environment.
-
-# Set paths.
-webroot=/app/drupal
-
-# Set Drush aliases.
-site=@uniarrival
-local=@uniarrival.local
-
-#Set default syncdb environment.
-env=prod
-
-function usage {
-  echo "Usage: $0 [prod]"
-  exit 1
-}
-
-if [ "$#" -gt 1 ]; then
-  usage
-fi
-
-if [ "$#" -eq 1 ]; then
-  if [ "$1" != "prod" ]; then
-    usage
-  fi
-  env=prod
-fi
-
-# Synchronize & sanitize database from selected environment.
-cd "$webroot"
-rm -rf /app/dump.sql
-drush "$local" sql-drop -y
-drush "$site.$env" sql-dump --structure-tables-list=cache,history,sessions,watchdog > /app/dump.sql
-drush "$local" sql-query --file=/app/dump.sql -y
-drush "$local" cc drush
-drush "$local" cr
-rm -rf /app/dump.sql
+# Synchronise local database with the production environment.
+cd /app/drupal
+drush sql-drop -y
+drush sql-sync @prod @local -y
+drush sqlsan -y
+drush cc drush
+drush cr
